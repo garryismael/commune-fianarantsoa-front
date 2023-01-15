c78488495404bb78aaebd0b55dadbf341426894a
Garry Ismaël <tahinjanaharygarry@gmail.com>
2023-01-15 12:15:24 +0300

    style topbar

--- src/components/TopBar/index.jsx
+++ src/components/TopBar/index.jsx
@@ -1,46 +1,46 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import PeopleImg from "../../assets/img/people.png";
 
 import "./index.css";
 
 function Topbar() {
+	const switchRef = useRef(null);
+
 	const onClick = () => {
 		const sidebar = window.document.getElementById("sidebar");
 		sidebar.classList.toggle("hide");
 	};
 
-	const onSwitch = () => {
-		const switchMode = window.document.getElementById("switch-mode");
-
-		switchMode.addEventListener("change", function () {
-			if (this.checked) {
-				document.body.classList.add("dark");
-			} else {
-				document.body.classList.remove("dark");
-			}
-		});
-	};
+	useEffect(() => {
+		if (switchRef && switchRef.current) {
+			switchRef.current.addEventListener("change", function () {
+				if (this.checked) {
+					document.body.classList.add("dark");
+				} else {
+					document.body.classList.remove("dark");
+				}
+			});
+		}
+	}, []);
 
 	return (
 		<nav className='navbar-content'>
-			<i className='bx bx-menu' onClick={onClick}></i>
-			<Link to='#' className='nav-link'>
-				Categories
-			</Link>
-			<form action='#'>
-				<div className='form-input'>
-					<input type='search' placeholder='Search...' />
-					<button type='submit' className='search-btn'>
-						<i className='bx bx-search'></i>
-					</button>
-				</div>
-			</form>
-			<input type='checkbox' id='switch-mode' hidden onChange={onSwitch}/>
-			<label htmlFor='switch-mode' className='switch-mode'></label>
-			<Link to='#' className='profile'>
-				<img src={PeopleImg} alt='' />
-			</Link>
+			<div className='content-left'>
+				<i className='bx bx-menu' onClick={onClick}></i>
+				<input
+					type='checkbox'
+					id='switch-mode'
+					ref={switchRef}
+					hidden
+				/>
+			</div>
+			<div className='content-right'>
+				<label htmlFor='switch-mode' className='switch-mode'></label>
+				<Link to='#' className='profile'>
+					<img src={PeopleImg} alt='' />
+				</Link>
+			</div>
 		</nav>
 	);
 }
